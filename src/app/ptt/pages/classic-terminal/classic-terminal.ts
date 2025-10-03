d48cc69a52b8aa072f1e56f5430758d13eaf8a19
Chris Weng <meetsuperyou@gmail.com>
2025-10-03 23:16:24 +0800

    我做了印出 buffer 的 test utils

---
+++ src/app/ptt/pages/classic-terminal/classic-terminal.ts
@@ -0,0 +1,12 @@
+import {CommonModule} from "@angular/common";
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'classic-terminal',
+  imports: [CommonModule],
+  templateUrl: './classic-terminal.html',
+  styleUrl: './classic-terminal.scss'
+})
+export class ClassicTerminal {
+
+}
---
+++ src/app/ptt/pages/terminal/terminal.ts
@@ -0,0 +1,32 @@
+import {CommonModule} from "@angular/common";
+import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+import {Cell} from "../../services/terminal/core/buffer-model";
+
+@Component({
+  selector: 'terminal',
+  imports: [CommonModule], // 導入 CommonModule 以使用 *ngFor, async pipe 等
+  templateUrl: './terminal.html',
+  styleUrl: './terminal.scss'
+})
+export class Terminal {
+  /**
+  public state$: Observable<TerminalState>;
+
+  constructor(private terminalBufferService: TerminalBufferService) {
+    this.state$ = this.terminalBufferService.state$;
+  }
+
+  // 輔助函式，用於計算儲存格的 ngStyle
+  getCellStyle(attrs: Cell): { [key: string]: any } {
+    const fg = attrs.inverse ? attrs.bg : attrs.fg;
+    const bg = attrs.inverse ? attrs.fg : attrs.bg;
+
+    const style: { [key: string]: any } = {};
+    if (fg) style['color'] = fg;
+    if (bg) style['backgroundColor'] = bg;
+
+    return style;
+  }
+  **/
+}
